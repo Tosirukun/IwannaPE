a0853a10de6df10f360a70b5adfe7b0422e7d1db
Tosirukun <tosirusan@gmail.com>
2020-10-02 16:22:09 +0900

    LICENSE UPDATE

--- setup.py
+++ setup.py
@@ -2,24 +2,25 @@
 
 # Learn more: https://github.com/kennethreitz/setup.py
 
-from setuptools import setup, find_packages
+from setuptools import setup
+from codecs import open
 
-
-with open('README.rst') as f:
+with open('README.md', 'r', encoding='utf-8') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
+with open('LICENSE', 'r', encoding='utf-8') as f:
     license = f.read()
 
 setup(
-    name='sample',
-    version='0.1.0',
-    description='Sample package for Python-Guide.org',
+    name='IwannaPE',
+    version='1.0.0',
+    description='Fast! Easy! Interesting!!! Easy create Iwanna Game',
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
+    author='Tosirukun',
+    author_email='tosirusan@gmail.com',
+    url='https://github.com/Tosirukun/IwannaPE',
     license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    install_requires=['pygame', 'asyncio'],
+    keywords='IwannaPE iwannape Tosirukun tosirukun IwannabethePythonEngine',
+    packages=['IwannaPE']
 )
-
