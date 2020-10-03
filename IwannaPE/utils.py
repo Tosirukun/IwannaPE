dd4630570af2872e72189439f01859798d995fdb
Tosirukun(とじるくん) <72246321+Tosirukun@users.noreply.github.com>
2020-10-03 20:50:35 +0900

    Update utils.py
    IwannaPE.utils.get(game, 指定名) でsurface等を取得できる関数getと
    IwannaPE.utils.valiable_get(game, 内部変数名) で変数を返すvaliable_get関数を追加
    
    valiable_getを使えるくらいプログラミングが出来たら多分valiable_get需要ないと思います(じゃあなぜ作ったし!!)

--- IwannaPE/utils.py
+++ IwannaPE/utils.py
@@ -4,3 +4,11 @@ class IndentError(Exception):
     
 class VariableError(Exception):
     print('渡す値が異常です')
+    pass
+
+def get(clss, valiable):
+    if valiable == "SURFACE" or valiable == "surface":
+        return clss.SURFACE
+    
+def valiable_get(clss, valiable_name):
+    eval('return {0}.{1}'.format(str(clss), str(valiable_name)))
